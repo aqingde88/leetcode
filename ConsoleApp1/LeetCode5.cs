951ebe551888c7caa3558a74b981ca6714b7ae31
unknown <361188502@qq.com>
2022-08-15 21:50:12 +0800

    bbb

--- ConsoleApp1/LeetCode5.cs
+++ ConsoleApp1/LeetCode5.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。*/
+ 
+
 namespace ConsoleApp1
 {
     class LeetCode5
@@ -19,15 +25,52 @@ namespace ConsoleApp1
             {
                 return false;
             }
-            List<char> leftCharacters = new List<char> { '(', '[', '{' };
-            List<char> rightCharacters = new List<char> { ')', ']', '}' };
-            return true;
+           
+
+            Dictionary<char, char> pairs = new Dictionary<char, char>();
+            pairs[')'] ='(';
+            pairs[']'] = '[';
+            pairs['}'] = '{';
+
+            List<char> chars = new List<char>();
+
+            for (int i = 0; i < len; i++)
+            {
+                var tmp = s[i];
+                //右括号
+               if(pairs.ContainsKey(tmp))
+               {
+                   if(chars.Count == 0)
+                    {
+                        return false;
+                    }
+                    if (chars[chars.Count - 1] == pairs[tmp])
+                    {
+                        chars.RemoveAt(chars.Count - 1);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+               }
+               else if (pairs.Values.Contains(tmp))
+               {
+                    chars.Add(tmp);
+               }
+               else
+                {
+                    return false;
+                }
+            }
+
+            return chars.Count == 0; 
         }
 
         
 
         public void Do()
         {
+            Console.WriteLine(IsValid("]"));
         }
 
     }
